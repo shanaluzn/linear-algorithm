82ddc18eaf1b8ff092925715ae164f91cc9da059
Shana Luz <shanaluz.napolitano@gmail.com>
2020-07-27 20:00:41 -0300

    exercise-one

---
+++ exercise-one/src/SumTwoNumbers.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class SumTwoNumbers {
+
+    public static void main(String[] args) {
+
+        // definición de variables
+        int firstNumber;
+        int secondNumber;
+        int result;
+
+        //habilito ingreso de datos por teclado
+        Scanner keyboard = new Scanner(System.in);
+
+        //leo datos de usuario
+        System.out.println("Ingrese el valor del primer número ");
+        firstNumber = keyboard.nextInt();
+        System.out.println("Ingrese el valor del segundo número ");
+        secondNumber = keyboard.nextInt();
+
+        //se hace el proceso y se guarda en la variable result
+        result = firstNumber + secondNumber;
+
+        // se muestra resultado
+        System.out.println("El resultado es: " +result);
+    }
+}
+
+
